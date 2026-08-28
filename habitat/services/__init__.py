59aa7aea3e2d090367313cb530727db19f834d6c
Nolane-x <casioreview20@gmail.com>
2026-08-28 22:12:04 +0700

    refactor: add lazy workspace service seams

---
+++ habitat/services/__init__.py
@@ -0,0 +1,11 @@
+from .index import IndexService
+from .query import QueryService
+from .runtime import RuntimeService
+from .transaction import TransactionService
+
+__all__ = [
+    "IndexService",
+    "QueryService",
+    "RuntimeService",
+    "TransactionService",
+]
