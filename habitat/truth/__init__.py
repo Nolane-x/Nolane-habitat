63d9fae4924d099a97d67d714fc865bf5399b2e2
Nolane-x <casioreview20@gmail.com>
2026-08-28 20:36:01 +0700

    feat: expose truth authority surface

---
+++ habitat/truth/__init__.py
@@ -0,0 +1,15 @@
+from .authority import (
+    AuthorityClass,
+    OperationAuthorityDeclaration,
+    legacy_authority,
+    operation_allows_evidence,
+    operation_authority,
+)
+
+__all__ = [
+    "AuthorityClass",
+    "OperationAuthorityDeclaration",
+    "legacy_authority",
+    "operation_allows_evidence",
+    "operation_authority",
+]
