2c89c863d23cafa3c021f3b1b53a8ba85395b6cb
Nolane-x <casioreview20@gmail.com>
2026-08-29 21:18:36 +0700

    feat: export Learning Plane domain kernel

---
+++ habitat/learning_plane/__init__.py
@@ -0,0 +1,19 @@
+"""Immutable domain contracts for Habitat Learning Plane V1."""
+
+from .model import (
+    CONSTITUTIONAL_LEARNING_TARGETS,
+    LEGAL_CANDIDATE_TRANSITIONS,
+    ContextPolicy,
+    EvaluationPacket,
+    OutcomeRecord,
+    PolicyCandidate,
+)
+
+__all__ = [
+    "CONSTITUTIONAL_LEARNING_TARGETS",
+    "LEGAL_CANDIDATE_TRANSITIONS",
+    "ContextPolicy",
+    "EvaluationPacket",
+    "OutcomeRecord",
+    "PolicyCandidate",
+]
